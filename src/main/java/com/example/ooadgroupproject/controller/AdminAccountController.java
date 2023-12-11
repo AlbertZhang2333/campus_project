b81cd86339e5a876126742f01db2eb3c2701506c
walltall <3077161150@qq.com>
2023-12-11 12:55:28 +0800

    整合现有资源

--- src/main/java/com/example/ooadgroupproject/controller/AdminAccountController.java
+++ src/main/java/com/example/ooadgroupproject/controller/AdminAccountController.java
@@ -2,20 +2,22 @@ package com.example.ooadgroupproject.controller;
 
 import com.example.ooadgroupproject.Utils.ManageAccountUtil;
 import com.example.ooadgroupproject.common.Result;
+import com.example.ooadgroupproject.entity.Account;
 import com.example.ooadgroupproject.service.AccountService;
 import com.example.ooadgroupproject.service.Impl.UploadFileServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
-import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/manageAccount")
-public class adminAccountController {
+public class AdminAccountController {
     @Autowired
     private AccountService accountService;
     @Autowired
@@ -38,4 +40,26 @@ public class adminAccountController {
             return Result.success("已成功将表内的用户创建");
         }
     }
+    @GetMapping("/checkAllAccount")
+    public List<Account> findAllAccount(){
+        return accountService.findAll();
+    }
+    @PostMapping("/setBlackList")
+    public Result setBlackList(long id){
+       Account account= accountService.getUserById(id);
+       account.setInBlackList(true);
+       return Result.success("已成功将"+id+"用户拉入黑名单");
+    }
+    @PostMapping("/releaseFromBlackList")
+    public Result releaseFromBlackList(long id){
+        Account account= accountService.getUserById(id);
+        if(account.isInBlackList()){
+            account.setInBlackList(false);
+            return Result.success("已成功将"+id+"用户从黑名单内释放");
+        }else {
+            return Result.fail("用户"+id+"不在黑名单内");
+        }
+    }
+
+
 }
